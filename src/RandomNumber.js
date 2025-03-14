d36b8403f197b76cf3e647bd342a24285a335395
hash_kim <willom2c@naver.com>
2025-03-15 00:14:37 +0900

    feat: 랜덤 숫자 클래스 생성

---
+++ src/RandomNumber.js
@@ -0,0 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export class RandomNumber {
+
+    #answer;
+
+    constructor() {
+        this.#answer = MissionUtils.Random.pickNumberInRange(1,999);
+    }
+
+    get printAnswer() {
+        return MissionUtils.Console.print(`Answer was : ${this.#answer}`);
+    }
+}

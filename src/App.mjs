d41d8f69dcf41fac392d31d5712622d13348451f
jay-so <merry9504@naver.com>
2025-03-15 22:57:10 +0900

    feat: 프로그램 입,출력 및 생성된 숫자와 사용자 숫자 비교 구현

--- src/App.mjs
+++ src/App.mjs
@@ -1,5 +1,16 @@
+import GameController from './controller/GameController.mjs';
+import ComputerNumberGenerator from './model/ComputerNumberGenerator.mjs';
+
 class App {
-  async play() {}
+  #gameController;
+
+  constructor() {
+    this.#gameController = new GameController(new ComputerNumberGenerator());
+  }
+
+  async play() {
+    await this.#gameController.startGame();
+  }
 }
 
 export default App;

8aabbd09250baadda177c6c7c00fd80c725bf48c
jay-so <merry9504@naver.com>
2025-03-15 19:53:54 +0900

    feat: 랜덤 3자리 숫자 생성 구현

---
+++ src/model/ComputerNumberGenerator.js
@@ -0,0 +1,39 @@
+import { Random } from '@woowacourse/mission-utils';
+import GAME_CONDITION from '../constant/conditions.mjs';
+
+class ComputerNumberGenerator {
+  #computerNumber;
+
+  constructor() {
+    this.#computerNumber = this.generateNumber();
+  }
+
+  /**
+   * 컴퓨터는 중복되지 않는 랜덤 세자리의 숫자를 생성한다.
+   * @returns {number} 중복되지 않는 세자리의 숫자
+   */
+
+  generateNumber() {
+    const digitalArray = new Set();
+
+    while (digitalArray.size < GAME_CONDITION.maxLength) {
+      const randomNumber = Random.pickNumberInRange(
+        GAME_CONDITION.startNumber,
+        GAME_CONDITION.endNumber
+      );
+      digitalArray.add(randomNumber);
+    }
+
+    return parseInt([...digitalArray].join(''), 10);
+  }
+
+  getComputerNumber() {
+    return this.#computerNumber;
+  }
+
+  initComputerNumber() {
+    this.#computerNumber = this.getComputerNumber;
+  }
+}
+
+export default ComputerNumberGenerator;

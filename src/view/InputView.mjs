303e39948cc2bff23a48d2dcc3258a34c9412256
jay-so <merry9504@naver.com>
2025-03-22 01:19:42 +0900

    feat: 잘못된 숫자 값에 대한 예외 처리 구현

--- src/view/InputView.mjs
+++ src/view/InputView.mjs
@@ -1,14 +1,22 @@
 import { Console } from '@woowacourse/mission-utils';
+import { MESSAGES } from '../constant/messages.mjs';
+import InputValidator from '../model/InputValidator.mjs';
 
 const InputView = {
   async getUserNumber(message) {
     const userNumber = await Console.readLineAsync(message);
+
+    if (!InputValidator.hasValidNumber(userNumber))
+      throw new Error(MESSAGES.inputError);
 
     return parseInt(userNumber, 10);
   },
 
   async getUserCommand(message) {
     const userCommand = await Console.readLineAsync(message);
+
+    if (!InputValidator.hasValidCommand(userCommand))
+      throw new Error(MESSAGES.inputError);
 
     return parseInt(userCommand, 10);
   },

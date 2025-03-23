f245e3288006e40d1218e610147df4a111125edc
jay-so <merry9504@naver.com>
2025-03-23 17:51:36 +0900

    chore: jest 설정 파일 분리

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  transform: {
+    '^.+\\.m?[tj]s$': 'babel-jest',
+  },
+  testMatch: [
+    '**/__tests__/**/*.js?(x)',
+    '**/?(*.)+(spec|test).js?(x)',
+    '**/PrintProgressTest.js',
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
